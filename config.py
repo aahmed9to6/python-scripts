3c3839016f35a9f9cef207dca6a5385c5a53bcb7
Abdullah Ahmed <abdullah.ahmed@coeus-solutions.de>
2018-12-13 20:28:56 +0500

    Update

--- config.py
+++ config.py
@@ -5,25 +5,16 @@ PASSWORD ='coeus123'
 BASE_DOWNLOAD_PATH = '/home/abdullah/Tuts/'
 SUBS = True
 COURSES = [
-	'photoshop-cc-2018-one-on-one-mastery',
-	'photoshop-cc-2018-one-on-one-advanced',
-	'photoshop-cc-2018-one-on-one-fundamentals',
-	'photoshop-tips-tricks-techniques-2',
-	'photoshop-selections-tips-tricks-and-techniques',
-	'magento-module-creation-and-customization-techniques',
-	'plangrid-managing-construction-activities',
-	'job-interview-strategies-for-ux-designers',
-	'developing-investment-acumen',
-	'azure-event-grid',
-	'microsoft-certifications-exams-paths-certifications-and-resources-2',
-	'bigcommerce-essential-training',
-	'windows-server-2019-active-directory-enterprise-infrastructure',
-	'python-data-structures-stacks-queues-and-deques',
-	'rhce-cert-prep-network-filesystems-using-nfs-and-samba',
-	'linux-foundation-cert-prep-network-filesystems-and-file-services-ubuntu'
-]
-
-WEEKLY = [
+	'learning-the-javascript-language',
+	'interpersonal-communication',
+	'learning-to-say-no',
+	'linux-foundation-cert-prep-system-design-and-deployment-ubuntu',
+	'rhce-cert-prep-mail-systems-using-postfix',
+	'javascript-classes-2',
+	'kali-linux-on-windows-10-first-look',
+	'css-animation',
+	'making-and-learning-from-mistakes',
+	'learning-python-gui-programming',
 	'excel-weekly-challenge',
 	'agile-challenges-weekly-tips',
 	'help-yourself-tech-tips-weekly',
