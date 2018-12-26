0d368a370a326bb430bd9543db5abcb6b0662289
Abdullah Ahmed <abdullah.ahmed@coeus-solutions.de>
2018-12-26 15:59:58 +0500

    Update

--- config.py
+++ config.py
@@ -5,19 +5,6 @@ PASSWORD ='coeus123'
 BASE_DOWNLOAD_PATH = '/home/abdullah/Tuts/'
 SUBS = True
 COURSES = [
-	'securing-the-iot-introduction',
-	'iot-foundations-low-power-wireless-networking',
-	'iot-development-with-windows-10-iot-core-1-installation-and-basic-connection',
-	'iot-development-with-windows-10-iot-core-2-enabling-the-cloud-and-security',
-	'android-app-development-internet-of-things',
-	'learning-arduino-interfacing-with-hardware',
-	'learning-arduino-pulse-width-modulation',
-	'learning-arduino-the-basics',
-	'learning-atom-2',
-	'learning-static-site-building-with-jekyll',
-	'learning-static-site-building-with-hugo-2',
-	'robot-framework-test-automation-jenkins-ci-and-git-version-control',
-	'learning-markdown',
 	'amazon-web-services-deploying-and-provisioning-2',
 	'dreamweaver-cc-responsive-design-with-bootstrap-4',
 	'python-gui-development-with-tkinter-2',
